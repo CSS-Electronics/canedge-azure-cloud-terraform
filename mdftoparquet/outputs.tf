46cb8c03debde8f706e68ffb16915d58b1bc8d75
mfcss <mf@csselectronics.com>
2025-08-02 14:33:35 +0200

    Adjustment to the backlog container deployment

--- mdftoparquet/outputs.tf
+++ mdftoparquet/outputs.tf
@@ -49,11 +49,11 @@ output "eventgrid_subscription_name" {
 }
 
 output "backlog_processor_job_name" {
-  description = "Name of the Backlog Processor Container App Job (if deployed)"
-  value       = var.deploy_backlog_processor && var.github_token != "" ? module.container_app_job_backlog[0].container_app_job_name : "not-deployed"
+  description = "Name of the backlog processor container app job"
+  value       = var.github_token != "" ? module.container_app_job_backlog[0].container_app_job_name : "not-deployed"
 }
 
 output "backlog_processor_environment_name" {
-  description = "Name of the Backlog Processor Container App Environment (if deployed)"
-  value       = var.deploy_backlog_processor && var.github_token != "" ? module.container_app_job_backlog[0].container_app_environment_name : "not-deployed"
+  description = "Name of the backlog processor container app environment"
+  value       = var.github_token != "" ? module.container_app_job_backlog[0].container_app_environment_name : "not-deployed"
 }
