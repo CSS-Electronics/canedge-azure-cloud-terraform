4fe03d4bf485b51f32da4b0f9f08e3e71e1513db
mfcss <mf@csselectronics.com>
2025-08-07 15:08:43 +0200

    no message

--- mdftoparquet/modules/container_app_job_aggregation/variables.tf
+++ mdftoparquet/modules/container_app_job_aggregation/variables.tf
@@ -23,7 +23,7 @@ variable "container_image" {
   description = "Container image to use for the job"
   type        = string
   # Try image without specific processor path
-  default     = "ghcr.io/css-electronics/canedge-mdftoparquet-automation/aggregation-processor:latest"
+  default     = "ghcr.io/css-electronics/canedge-mdftoparquet-automation/aggregation-processor:sha-8403267"
 }
 
 variable "storage_account_name" {
