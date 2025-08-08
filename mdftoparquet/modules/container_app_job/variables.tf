01fca8793420777e24ed55611d5fe2c9e6520163
mfcss <mf@csselectronics.com>
2025-08-08 15:36:33 +0200

    Update to container sha a626ab2

--- mdftoparquet/modules/container_app_job/variables.tf
+++ mdftoparquet/modules/container_app_job/variables.tf
@@ -22,7 +22,7 @@ variable "job_name" {
 variable "container_image" {
   description = "Container image to use for the job"
   type        = string
-  default     = "ghcr.io/css-electronics/canedge-mdftoparquet-automation/backlog-processor:sha-0bc2afc"
+  default     = "ghcr.io/css-electronics/canedge-mdftoparquet-automation/backlog-processor:sha-a626ab2"
 }
 
 variable "storage_account_name" {
