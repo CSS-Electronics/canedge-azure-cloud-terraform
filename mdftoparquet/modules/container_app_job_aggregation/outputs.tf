8ec09c5826843e420b9dbb9637aa604816bcd7fd
mfcss <mf@csselectronics.com>
2025-08-08 12:07:36 +0200

    Updated contianer references

--- mdftoparquet/modules/container_app_job_aggregation/outputs.tf
+++ mdftoparquet/modules/container_app_job_aggregation/outputs.tf
@@ -17,3 +17,13 @@ output "execution_command" {
   description = "CLI command to manually execute the job"
   value       = "az containerapp job start --name ${azurerm_container_app_job.process_aggregation.name} --resource-group ${var.resource_group_name}"
 }
+
+output "job_id" {
+  description = "ID of the Container App Job"
+  value       = azurerm_container_app_job.process_aggregation.id
+}
+
+output "endpoint" {
+  description = "Endpoint URL for the Container App Job"
+  value       = "https://${azurerm_container_app_environment.job_env.name}.${azurerm_container_app_environment.job_env.location}.azurecontainerapps.io"
+}
